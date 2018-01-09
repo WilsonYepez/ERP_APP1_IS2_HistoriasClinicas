4cb34a92533cbf8b55aa758fbf6b4910a0a46286
JHONNY JOSE TERAN CHANCUSIG <jjteranc@utn.edu.ec>
2018-01-09 15:11:30 -0500

    v0.6.2 crud empleados

--- ERP_APP1_IS2_HistoriasClinicas/ERP_APP1_IS2_HistoriasClinicas/RecursosHumanosLS.cs
+++ ERP_APP1_IS2_HistoriasClinicas/ERP_APP1_IS2_HistoriasClinicas/RecursosHumanosLS.cs
@@ -85,5 +85,156 @@ namespace ERP_APP1_IS2_HistoriasClinicas
             }
             return 0;
         }
+
+        public Empleados2[] lista_empleados()       //LISTADO DE EMPLEADOS COMPLETO
+        {
+            using (var ctx = new ClinicaHistoriaEntities1())
+            {
+                var lst = from d in ctx.EMPLEADOS
+                          select d;
+                Empleados2[] r = new Empleados2[lst.Count()];
+                int i = 0;
+                foreach (var d in lst)
+                {
+                    r[i++] = new Empleados2
+                    {
+                        CEDULA_EMP = d.CEDULA_EMP,
+                        ID_AREA = d.ID_AREA,
+                        NOMBRES_EMP = d.NOMBRES_EMP,
+                        NACIFECHA_EMP = d.NACIFECHA_EMP,
+                        NACIONALIDAD_EMP = d.NACIONALIDAD_EMP,
+                        DIRECCIO_EMP = d.DIRECCIO_EMP,
+                        TELEFONO_EMP = d.TELEFONO_EMP,
+                        EMAIL_EMP = d.EMAIL_EMP,
+                        ESTADO_EMP = d.ESTADO_EMP,
+                        TIPO_EMP = d.TIPO_EMP,
+                        ACTIVO_EMP = d.ACTIVO_EMP,
+                        ID_H = d.ID_H,
+                        CIUDADNACI_EMP = d.CIUDADNACI_EMP
+                    };
+                }
+                return r;
+            }
+        }
+        public Empleados2[] lista_empleados_AREATRABAJO(decimal IDAREA)     //EMPLEADOS POR AREA DE TRABAJO
+        {
+            using (var ctx = new ClinicaHistoriaEntities1())
+            {
+                var lst = from d in ctx.EMPLEADOS
+                          where d.ID_AREA == IDAREA
+                          select d;
+                Empleados2[] r = new Empleados2[lst.Count()];
+                int i = 0;
+                foreach (var d in lst)
+                {
+                    r[i++] = new Empleados2
+                    {
+                        CEDULA_EMP = d.CEDULA_EMP,
+                        ID_AREA = d.ID_AREA,
+                        NOMBRES_EMP = d.NOMBRES_EMP,
+                        NACIFECHA_EMP = d.NACIFECHA_EMP,
+                        NACIONALIDAD_EMP = d.NACIONALIDAD_EMP,
+                        DIRECCIO_EMP = d.DIRECCIO_EMP,
+                        TELEFONO_EMP = d.TELEFONO_EMP,
+                        EMAIL_EMP = d.EMAIL_EMP,
+                        ESTADO_EMP = d.ESTADO_EMP,
+                        TIPO_EMP = d.TIPO_EMP,
+                        ACTIVO_EMP = d.ACTIVO_EMP,
+                        ID_H = d.ID_H,
+                        CIUDADNACI_EMP = d.CIUDADNACI_EMP
+                    };
+                }
+                return r;
+            }
+        }
+        public Empleados2 empleado_CEDULA(string CEDULA_EMP)        //EMPLEADOS  BUSCAR POR CEDULA
+        {
+            using (var ctx = new ClinicaHistoriaEntities1())
+            {
+                var d = ctx.EMPLEADOS.Find(CEDULA_EMP);
+                Empleados2 r = null;
+                if (d != null)
+                {
+                    r = new Empleados2
+                    {
+                        CEDULA_EMP = d.CEDULA_EMP,
+                        ID_AREA = d.ID_AREA,
+                        NOMBRES_EMP = d.NOMBRES_EMP,
+                        NACIFECHA_EMP = d.NACIFECHA_EMP,
+                        NACIONALIDAD_EMP = d.NACIONALIDAD_EMP,
+                        DIRECCIO_EMP = d.DIRECCIO_EMP,
+                        TELEFONO_EMP = d.TELEFONO_EMP,
+                        EMAIL_EMP = d.EMAIL_EMP,
+                        ESTADO_EMP = d.ESTADO_EMP,
+                        TIPO_EMP = d.TIPO_EMP,
+                        ACTIVO_EMP = d.ACTIVO_EMP,
+                        ID_H = d.ID_H,
+                        CIUDADNACI_EMP = d.CIUDADNACI_EMP
+                    };
+                }
+                return r;
+            }
+        }
+        public int insertar_empleado(string CEDULA_EMP, decimal ID_AREA, decimal ID_H, string NOMBRES_EMP, System.DateTime NACIFECHA_EMP, string NACIONALIDAD_EMP, string DIRECCIO_EMP, string TELEFONO_EMP, string EMAIL_EMP, string ESTADO_EMP, string TIPO_EMP, string ACTIVO_EMP, string CIUDADNACI_EMP)
+        {                                                       //INSERTAR EMPLEADOS
+            using (var ctx = new ClinicaHistoriaEntities1())
+            {
+                var d = new EMPLEADOS();
+                d.CEDULA_EMP = CEDULA_EMP;
+                d.ID_AREA = ID_AREA;
+                d.ID_H = ID_H;
+                d.NOMBRES_EMP = NOMBRES_EMP;
+                d.NACIFECHA_EMP = NACIFECHA_EMP;
+                d.NACIONALIDAD_EMP = NACIONALIDAD_EMP;
+                d.DIRECCIO_EMP = DIRECCIO_EMP;
+                d.TELEFONO_EMP = TELEFONO_EMP;
+                d.EMAIL_EMP = EMAIL_EMP;
+                d.ESTADO_EMP = ESTADO_EMP;
+                d.TIPO_EMP = TIPO_EMP;
+                d.ACTIVO_EMP = ACTIVO_EMP;
+                d.CIUDADNACI_EMP = CIUDADNACI_EMP;
+                ctx.EMPLEADOS.Add(d);
+                ctx.SaveChanges();
+            }
+            return 0;
+        }
+        public int update_empleado(string CEDULA_EMP, decimal ID_AREA, decimal ID_H, string NOMBRES_EMP, System.DateTime NACIFECHA_EMP, string NACIONALIDAD_EMP, string DIRECCIO_EMP, string TELEFONO_EMP, string EMAIL_EMP, string ESTADO_EMP, string TIPO_EMP, string ACTIVO_EMP, string CIUDADNACI_EMP)
+        {                                                       //UPDATE EMPLEADO
+            using (var ctx = new ClinicaHistoriaEntities1())
+            {
+                var d = ctx.EMPLEADOS.Find(CEDULA_EMP);
+                if (d != null)
+                {
+                    d.ID_AREA = ID_AREA;
+                    d.ID_H = ID_H;
+                    d.NOMBRES_EMP = NOMBRES_EMP;
+                    d.NACIFECHA_EMP = NACIFECHA_EMP;
+                    d.NACIONALIDAD_EMP = NACIONALIDAD_EMP;
+                    d.DIRECCIO_EMP = DIRECCIO_EMP;
+                    d.TELEFONO_EMP = TELEFONO_EMP;
+                    d.EMAIL_EMP = EMAIL_EMP;
+                    d.ESTADO_EMP = ESTADO_EMP;
+                    d.TIPO_EMP = TIPO_EMP;
+                    d.ACTIVO_EMP = ACTIVO_EMP;
+                    d.CIUDADNACI_EMP = CIUDADNACI_EMP;
+                }
+                ctx.SaveChanges();
+            }
+            return 0;
+        }
+        public int delete_empleado(string CEDULA_EMP)       //BORRAR EMPLEADO
+        {
+            using (var ctx = new ClinicaHistoriaEntities1())
+            {
+                var d = ctx.EMPLEADOS.Find(CEDULA_EMP);
+                if (d != null)
+                {
+                    ctx.EMPLEADOS.Remove(d);
+                }
+            }
+            return 0;
+        }
     }
+
+
 }
