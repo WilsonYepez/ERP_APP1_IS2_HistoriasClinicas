fc3304b9d6f45450bd68119d0c81be9820054b52
WILSON RAUL YEPEZ PONCE <wryepezp@utn.edu.ec>
2018-01-10 05:19:37 -0500

    v1.10 Web Service

--- ERP_HistoriasClinicas_ServiciosDistribuidos/App_Code/WebService.cs
+++ ERP_HistoriasClinicas_ServiciosDistribuidos/App_Code/WebService.cs
@@ -21,10 +21,11 @@ public class WebService : System.Web.Services.WebService
         //InitializeComponent(); 
     }
 
+    //Recursos Humanos
     [WebMethod]
-    public string HelloWorld()
+    public int Insertar_Area(decimal ID_AREA, string NOMBRE_AREA, string GERENCIA_AREA, string DESCRIPCION_AREA, string ESTADO_AREA)
     {
-        return "Hola a todos";
+        return new ERP_HistoriasClinicas.RecursosHumanos_LS().insertar_area(ID_AREA, NOMBRE_AREA, GERENCIA_AREA, DESCRIPCION_AREA, ESTADO_AREA);
     }
     [WebMethod]
     public ERP_HistoriasClinicas.Area_trabajo2[] Listado_Areas()
@@ -32,10 +33,105 @@ public class WebService : System.Web.Services.WebService
         return new ERP_HistoriasClinicas.RecursosHumanos_LS().Listado_Areas();
     }
     [WebMethod]
-    public int Insertar_Area(decimal ID_AREA, string NOMBRE_AREA, string GERENCIA_AREA, string DESCRIPCION_AREA, string ESTADO_AREA)
+    public ERP_HistoriasClinicas.Area_trabajo2[] Listado_Areas_Nombre(String NOMBRE_AREA)
     {
-        return new ERP_HistoriasClinicas.RecursosHumanos_LS().insertar_area(ID_AREA, NOMBRE_AREA,GERENCIA_AREA,DESCRIPCION_AREA,ESTADO_AREA);
+        return new ERP_HistoriasClinicas.RecursosHumanos_LS().lista_areas_nombre(NOMBRE_AREA);
     }
+    [WebMethod]
+    public ERP_HistoriasClinicas.Area_trabajo2 Listado_Area(decimal ID_AREA)
+    {
+        return new ERP_HistoriasClinicas.RecursosHumanos_LS().area_idArea(ID_AREA);
+    }
+    [WebMethod]
+    public int insertar_area(decimal ID_AREA, string NOMBRE_AREA, string GERENCIA_AREA, string DESCRIPCION_AREA, string ESTADO_AREA)
+    {
+        return new ERP_HistoriasClinicas.RecursosHumanos_LS().insertar_area( ID_AREA,  NOMBRE_AREA,  GERENCIA_AREA,DESCRIPCION_AREA,  ESTADO_AREA);
+    }
+    [WebMethod]
+    public int actualizar_area(Decimal ID_AREA, string NOMBRE_AREA, string GERENCIA_AREA, string DESCRIPCION_AREA, string ESTADO_AREA)
+    {
+        return new ERP_HistoriasClinicas.RecursosHumanos_LS().actualizar_area( ID_AREA,  NOMBRE_AREA, GERENCIA_AREA,  DESCRIPCION_AREA, ESTADO_AREA);
+    }
+    [WebMethod]
+    public int delete_area(decimal ID_AREA)
+    {
+        return new ERP_HistoriasClinicas.RecursosHumanos_LS().delete_area(ID_AREA);
+    }
+
+[WebMethod]
+public ERP_HistoriasClinicas.Empleado2[] Listado_Empleados()
+    {
+        return new ERP_HistoriasClinicas.RecursosHumanos_LS().Listado_Empleados();
+    }
+[WebMethod]
+public ERP_HistoriasClinicas.Empleado2 Empleado_Cedula(string CEDULA_EMP)
+    {
+        return new ERP_HistoriasClinicas.RecursosHumanos_LS().Empleado_Cedula(CEDULA_EMP);
+    }
+
+    [WebMethod]
+public int insertar_Empleado(string CEDULA_EMP, decimal ID_AREA, string CARGO_US, string PASWORD_US)
+    {
+        return new ERP_HistoriasClinicas.RecursosHumanos_LS().insertar_Empleado(CEDULA_EMP, ID_AREA, CARGO_US, PASWORD_US);
+    }
+
+    [WebMethod]
+public int update_empleado(string CEDULA_EMP, decimal ID_AREA, string CARGO_US, string PASWORD_US)
+    {
+        return new ERP_HistoriasClinicas.RecursosHumanos_LS().update_empleado(CEDULA_EMP, ID_AREA, CARGO_US,PASWORD_US);
+    }
+
+    [WebMethod]
+public int delete_empleado(string CEDULA_EMP)
+    {
+        return new ERP_HistoriasClinicas.RecursosHumanos_LS().delete_empleado(CEDULA_EMP);
+    }
+
+    [WebMethod]
+public ERP_HistoriasClinicas.Persona2 Persona_Cedula(string CEDULA_EMP)
+    {
+        return new ERP_HistoriasClinicas.RecursosHumanos_LS().Persona_Cedula( CEDULA_EMP);
+    }
+
+    [WebMethod]
+public ERP_HistoriasClinicas.Persona2[] Listado_Persona()
+{
+        return new ERP_HistoriasClinicas.RecursosHumanos_LS().Listado_Persona();
+    }
+
+    [WebMethod]
+public int update_Persona(string CEDULA_EMP, string NOMBRES_EMP, System.DateTime NACIFECHA_EMP, string NACIONALIDAD_EMP, string DIRECCIO_EMP,
+            string TELEFONO_EMP, string EMAIL_EMP, string ESTADO_EMP, string TIPO_EMP, string ACTIVO_EMP, string CIUDADNACI_EMP)
+    {
+        return new ERP_HistoriasClinicas.RecursosHumanos_LS().update_Persona(CEDULA_EMP, NOMBRES_EMP, NACIFECHA_EMP, NACIONALIDAD_EMP,  DIRECCIO_EMP,
+            TELEFONO_EMP,  EMAIL_EMP,  ESTADO_EMP,  TIPO_EMP,  ACTIVO_EMP,  CIUDADNACI_EMP);
+    }
+
+    [WebMethod]
+public int insertar_Persona(string CEDULA_EMP, string NOMBRES_EMP, System.DateTime NACIFECHA_EMP, string NACIONALIDAD_EMP, string DIRECCIO_EMP,
+            string TELEFONO_EMP, string EMAIL_EMP, string ESTADO_EMP, string TIPO_EMP, string ACTIVO_EMP, string CIUDADNACI_EMP)
+{
+        return new ERP_HistoriasClinicas.RecursosHumanos_LS().insertar_Persona(CEDULA_EMP,  NOMBRES_EMP,  NACIFECHA_EMP,  NACIONALIDAD_EMP,  DIRECCIO_EMP,
+             TELEFONO_EMP,  EMAIL_EMP,  ESTADO_EMP, TIPO_EMP,  ACTIVO_EMP,  CIUDADNACI_EMP);
+    }
+
+    [WebMethod]
+public  int delete_Persona(string CEDULA_EMP)
+    {
+        return new ERP_HistoriasClinicas.RecursosHumanos_LS().delete_Persona( CEDULA_EMP);
+    }
+
+    [WebMethod]
+public ERP_HistoriasClinicas.Listado_facturacion2[] Listado_Facturacion()
+    {
+        return new ERP_HistoriasClinicas.RecursosHumanos_LS().Listado_Facturacion();
+    }
+
+
+
+    //Consultorio
+
+
 
     [WebMethod]
     public ERP_HistoriasClinicas.Historias2[] listado_historia()
