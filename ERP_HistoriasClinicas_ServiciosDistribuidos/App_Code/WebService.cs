7fe44829dae3e32b0d7d7a18ed0054f5b6c0ceb5
WILSON RAUL YEPEZ PONCE <wryepezp@utn.edu.ec>
2018-01-10 05:35:47 -0500

    v1.11 Web Service Consultorio

--- ERP_HistoriasClinicas_ServiciosDistribuidos/App_Code/WebService.cs
+++ ERP_HistoriasClinicas_ServiciosDistribuidos/App_Code/WebService.cs
@@ -130,9 +130,7 @@ public ERP_HistoriasClinicas.Listado_facturacion2[] Listado_Facturacion()
 
 
     //Consultorio
-
-
-
+    
     [WebMethod]
     public ERP_HistoriasClinicas.Historias2[] listado_historia()
     {
@@ -149,5 +147,79 @@ public ERP_HistoriasClinicas.Listado_facturacion2[] Listado_Facturacion()
                     VIVO_H_H, MUERTE_H_H, CAUSAS_H_H, SON_H, VIVO_SON_H, MUERTOS_SON_H, CAUSAS_SON_H, ALCOHOL_H, TABACO_H,
                     DROGAS_H, FRECUENCIA_TAB_H, FRECUENCIA_ALC_H, FRECUENCIA_DRO_H, ALIMENTACION_H, DIPSIA_H,
                     PARADIPSIA, FRECUENCIA_PARADIPSIA, INSOMNIO_H, FRECUENCIA_INSOM_H, OTRO_H);
+    }
+    [WebMethod]
+    public ERP_HistoriasClinicas.Historias2 una_historia(string CEDULA_EMP)
+    {
+        return new ERP_HistoriasClinicas.Consultorio().una_historia( CEDULA_EMP);
+    }
+    [WebMethod]
+    public int actualizar_hitoria(string CEDULA_EMP, string PADRE_H, string MADRE_H, string FALLECIDO_P_H, string FALLECIDO_M_H, string CAUSAS_P_H, string CAUSAS_M_H, decimal NUM_H_H,
+                    decimal VIVO_H_H, decimal MUERTE_H_H, string CAUSAS_H_H, decimal SON_H, decimal VIVO_SON_H, decimal MUERTOS_SON_H, string CAUSAS_SON_H, string ALCOHOL_H, string TABACO_H,
+                    string DROGAS_H, string FRECUENCIA_TAB_H, string FRECUENCIA_ALC_H, string FRECUENCIA_DRO_H, string ALIMENTACION_H, string DIPSIA_H,
+                    string PARADIPSIA, string FRECUENCIA_PARADIPSIA, string INSOMNIO_H, string FRECUENCIA_INSOM_H, string OTRO_H)
+    {
+        return new ERP_HistoriasClinicas.Consultorio().actualizar_hitoria( CEDULA_EMP,  PADRE_H,  MADRE_H,  FALLECIDO_P_H, FALLECIDO_M_H,  CAUSAS_P_H,  CAUSAS_M_H, NUM_H_H,
+                     VIVO_H_H,  MUERTE_H_H,  CAUSAS_H_H,  SON_H,  VIVO_SON_H, MUERTOS_SON_H,  CAUSAS_SON_H,  ALCOHOL_H, TABACO_H,
+                     DROGAS_H,  FRECUENCIA_TAB_H,  FRECUENCIA_ALC_H,  FRECUENCIA_DRO_H,  ALIMENTACION_H,  DIPSIA_H,
+                     PARADIPSIA,  FRECUENCIA_PARADIPSIA, INSOMNIO_H, FRECUENCIA_INSOM_H,  OTRO_H);
+    }
+    [WebMethod]
+    public ERP_HistoriasClinicas.Cabecera_consulta2[] lista_consulta()
+    {
+        return new ERP_HistoriasClinicas.Consultorio().lista_consulta();
+    }
+    [WebMethod]
+    public ERP_HistoriasClinicas.Cabecera_consulta2 una_consulta(int ID_CON)
+    {
+        return new ERP_HistoriasClinicas.Consultorio().una_consulta( ID_CON);
+    }
+    [WebMethod]
+    public int insertar_consulta(Decimal ID_CON, string CEDULA_EMP, string INDICACIONES_CON, string MOTIVO_CON, Decimal PRESION_CON,
+                            DateTime FECHA_CON, Decimal PESO_CON, Decimal TEMPERATURA_CON, string SINTOMAS_CON, string OBSERVACIONES_CON)
+    {
+        return new ERP_HistoriasClinicas.Consultorio().insertar_consulta( ID_CON,  CEDULA_EMP, INDICACIONES_CON, MOTIVO_CON,PRESION_CON,
+                             FECHA_CON,  PESO_CON,  TEMPERATURA_CON,  SINTOMAS_CON,  OBSERVACIONES_CON);
+    }
+    [WebMethod]
+    public int actualizar_consulta(Decimal ID_CON, string CEDULA_EMP, string INDICACIONES_CON, string MOTIVO_CON, Decimal PRESION_CON,
+                            DateTime FECHA_CON, Decimal PESO_CON, Decimal TEMPERATURA_CON, string SINTOMAS_CON, string OBSERVACIONES_CON)
+    {
+        return new ERP_HistoriasClinicas.Consultorio().actualizar_consulta( ID_CON, CEDULA_EMP,  INDICACIONES_CON,  MOTIVO_CON,  PRESION_CON,
+                             FECHA_CON,  PESO_CON, TEMPERATURA_CON,  SINTOMAS_CON,  OBSERVACIONES_CON);
+    }
+    [WebMethod]
+    public int borrar_consulta(Decimal ID_CON)
+    {
+        return new ERP_HistoriasClinicas.Consultorio().borrar_consulta( ID_CON);
+    }
+    [WebMethod]
+    public ERP_HistoriasClinicas.Detalle_Consulta2[] listado_detalle()
+    {
+        return new ERP_HistoriasClinicas.Consultorio().listado_detalle();
+    }
+    [WebMethod]
+    public ERP_HistoriasClinicas.Detalle_Consulta2 un_detalle(string ID_DET)
+    {
+        return new ERP_HistoriasClinicas.Consultorio().un_detalle(ID_DET);
+    }
+    [WebMethod]
+    public int insertar_detalle(string ID_DET, Decimal ID_CON, Decimal CANTIDAD_DET, Decimal PVP_DET,
+                        Decimal CANTDES_DET)
+    {
+        return new ERP_HistoriasClinicas.Consultorio().insertar_detalle( ID_DET, ID_CON,  CANTIDAD_DET,  PVP_DET,
+                        CANTDES_DET);
+    }
+    [WebMethod]
+    public int actualizar_detalle(string ID_DET, Decimal ID_CON, Decimal CANTIDAD_DET, Decimal PVP_DET,
+                        Decimal CANTDES_DET)
+    {
+        return new ERP_HistoriasClinicas.Consultorio().actualizar_detalle(ID_DET,ID_CON, CANTIDAD_DET, PVP_DET,
+                        CANTDES_DET);
+    }
+    [WebMethod]
+    public int borrar_detalle(string ID_DET)
+    {
+        return new ERP_HistoriasClinicas.Consultorio().borrar_detalle(ID_DET);
     }
 }
