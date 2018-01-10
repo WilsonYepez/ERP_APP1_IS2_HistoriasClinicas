aa1f55de371cd5dbedccc4aa7ad8a8823877a20a
WILMER ALEXANDER OJEDA ESCOBAR <waojedae@utn.edu.ec>
2018-01-10 04:49:25 -0500

    correccion modelo

--- ERP_HistoriasClinicas/ERP_HistoriasClinicas/Consultorio.cs
+++ ERP_HistoriasClinicas/ERP_HistoriasClinicas/Consultorio.cs
@@ -98,18 +98,47 @@ namespace ERP_HistoriasClinicas
             }
         }
 
-        public int insertar_historia(string CEDULA_EMP, string PADRE_H, string MADRE_H, string FALLECIDO_P_H, string FALLECIDO_M_H, string CAUSAS_P_H, string CAUSAS_M_H, decimal NUM_H_H,
-                    decimal VIVO_H_H, decimal MUERTE_H_H, string CAUSAS_H_H, decimal SON_H, decimal VIVO_SON_H, decimal MUERTOS_SON_H, string CAUSAS_SON_H, string ALCOHOL_H, string TABACO_H,
+        public int insertar_historia(string CEDULA_EMP, string PADRE_H, string MADRE_H, string FALLECIDO_P_H, string FALLECIDO_M_H, string CAUSAS_P_H, string CAUSAS_M_H, Decimal NUM_H_H,
+                    Decimal VIVO_H_H, Decimal MUERTE_H_H, string CAUSAS_H_H, Decimal SON_H, Decimal VIVO_SON_H, Decimal MUERTOS_SON_H, string CAUSAS_SON_H, string ALCOHOL_H, string TABACO_H,
                     string DROGAS_H, string FRECUENCIA_TAB_H, string FRECUENCIA_ALC_H, string FRECUENCIA_DRO_H, string ALIMENTACION_H, string DIPSIA_H,
                     string PARADIPSIA, string FRECUENCIA_PARADIPSIA, string INSOMNIO_H, string FRECUENCIA_INSOM_H, string OTRO_H)
         {
             using (var ctx = new ERP_HistoriasClinicasEntities())
             {
-                ctx.trx_Insert_Historia(CEDULA_EMP,PADRE_H,MADRE_H,FALLECIDO_P_H,FALLECIDO_M_H,CAUSAS_P_H,CAUSAS_M_H,NUM_H_H,VIVO_H_H,MUERTE_H_H,CAUSAS_H_H,
-                    SON_H,VIVO_SON_H,MUERTOS_SON_H,CAUSAS_SON_H,ALCOHOL_H,TABACO_H,DROGAS_H,FRECUENCIA_TAB_H,FRECUENCIA_ALC_H,FRECUENCIA_DRO_H,ALIMENTACION_H,DIPSIA_H,
-                    PARADIPSIA,FRECUENCIA_PARADIPSIA,INSOMNIO_H,FRECUENCIA_INSOM_H,OTRO_H);
-                return 0;
+                var d = new HISTORIAS();
+                d.CEDULA_EMP = d.CEDULA_EMP;
+                d.PADRE_H = PADRE_H;
+                d.MADRE_H = MADRE_H;
+                d.FALLECIDO_P_H = FALLECIDO_P_H;
+                d.FALLECIDO_M_H = FALLECIDO_M_H;
+                d.CAUSAS_P_H = CAUSAS_P_H;
+                d.CAUSAS_M_H = CAUSAS_M_H;
+                d.NUM_H_H = NUM_H_H;
+                d.VIVO_H_H = VIVO_H_H;
+                d.MUERTE_H_H = MUERTE_H_H;
+                d.CAUSAS_H_H = CAUSAS_H_H;
+                d.SON_H = SON_H;
+                d.VIVO_SON_H = VIVO_SON_H;
+                d.MUERTOS_SON_H = MUERTOS_SON_H;
+                d.CAUSAS_SON_H = CAUSAS_SON_H;
+                d.ALCOHOL_H = ALCOHOL_H;
+                d.TABACO_H = TABACO_H;
+                d.DROGAS_H = DROGAS_H;
+                d.FRECUENCIA_TAB_H = FRECUENCIA_TAB_H;
+                d.FRECUENCIA_ALC_H = FRECUENCIA_ALC_H;
+                d.FRECUENCIA_DRO_H = FRECUENCIA_DRO_H;
+                d.ALIMENTACION_H = ALIMENTACION_H;
+                d.DIPSIA_H = DIPSIA_H;
+                d.PARADIPSIA = PARADIPSIA;
+                d.FRECUENCIA_PARADIPSIA = FRECUENCIA_PARADIPSIA;
+                d.INSOMNIO_H = INSOMNIO_H;
+                d.FRECUENCIA_INSOM_H = FRECUENCIA_INSOM_H;
+                d.OTRO_H = OTRO_H;
+
+                ctx.HISTORIAS.Add(d);
+                ctx.SaveChanges();
             }
+            return 0;
         }
 
         public int actualizar_hitoria(string CEDULA_EMP, string PADRE_H, string MADRE_H, string FALLECIDO_P_H, string FALLECIDO_M_H, string CAUSAS_P_H, string CAUSAS_M_H, decimal NUM_H_H,
--- ERP_HistoriasClinicas_ServiciosDistribuidos/App_Code/WebService.cs
+++ ERP_HistoriasClinicas_ServiciosDistribuidos/App_Code/WebService.cs
@@ -37,4 +37,21 @@ public class WebService : System.Web.Services.WebService
         return new ERP_HistoriasClinicas.RecursosHumanos_LS().insertar_area(ID_AREA, NOMBRE_AREA,GERENCIA_AREA,DESCRIPCION_AREA,ESTADO_AREA);
     }
 
+    [WebMethod]
+    public ERP_HistoriasClinicas.Historias2[] listado_historia()
+    {
+        return new ERP_HistoriasClinicas.Consultorio().listado_historias();
+    }
+
+    [WebMethod]
+    public int insertar_historia(string CEDULA_EMP, string PADRE_H, string MADRE_H, string FALLECIDO_P_H, string FALLECIDO_M_H, string CAUSAS_P_H, string CAUSAS_M_H, Decimal NUM_H_H,
+                    Decimal VIVO_H_H, Decimal MUERTE_H_H, string CAUSAS_H_H, Decimal SON_H, Decimal VIVO_SON_H, Decimal MUERTOS_SON_H, string CAUSAS_SON_H, string ALCOHOL_H, string TABACO_H,
+                    string DROGAS_H, string FRECUENCIA_TAB_H, string FRECUENCIA_ALC_H, string FRECUENCIA_DRO_H, string ALIMENTACION_H, string DIPSIA_H,
+                    string PARADIPSIA, string FRECUENCIA_PARADIPSIA, string INSOMNIO_H, string FRECUENCIA_INSOM_H, string OTRO_H)
+    {
+        return new ERP_HistoriasClinicas.Consultorio().insertar_historia(CEDULA_EMP, PADRE_H, MADRE_H, FALLECIDO_P_H, FALLECIDO_M_H, CAUSAS_P_H, CAUSAS_M_H, NUM_H_H,
+                    VIVO_H_H, MUERTE_H_H, CAUSAS_H_H, SON_H, VIVO_SON_H, MUERTOS_SON_H, CAUSAS_SON_H, ALCOHOL_H, TABACO_H,
+                    DROGAS_H, FRECUENCIA_TAB_H, FRECUENCIA_ALC_H, FRECUENCIA_DRO_H, ALIMENTACION_H, DIPSIA_H,
+                    PARADIPSIA, FRECUENCIA_PARADIPSIA, INSOMNIO_H, FRECUENCIA_INSOM_H, OTRO_H);
+    }
 }
