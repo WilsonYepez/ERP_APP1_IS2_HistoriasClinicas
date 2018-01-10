5ab0f9b2d386bd182f887d97b7cd180bb0aa6e7b
WILSON RAUL YEPEZ PONCE <wryepezp@utn.edu.ec>
2018-01-10 04:14:19 -0500

    Pausa

--- ERP_HistoriasClinicas_ServiciosDistribuidos2/App_Code/WebService_Historia.cs
+++ ERP_HistoriasClinicas_ServiciosDistribuidos2/App_Code/WebService_Historia.cs
@@ -5,16 +5,16 @@ using System.Web;
 using System.Web.Services;
 
 /// <summary>
-/// Descripción breve de WebService
+/// Descripción breve de WebService_Historia
 /// </summary>
 [WebService(Namespace = "http://tempuri.org/")]
 [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
 // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
 // [System.Web.Script.Services.ScriptService]
-public class WebService : System.Web.Services.WebService
+public class WebService_Historia : System.Web.Services.WebService
 {
 
-    public WebService()
+    public WebService_Historia()
     {
 
         //Elimine la marca de comentario de la línea siguiente si utiliza los componentes diseñados 
@@ -27,14 +27,9 @@ public class WebService : System.Web.Services.WebService
         return "Hola a todos";
     }
     [WebMethod]
-    public ERP_HistoriasClinicas.Area_trabajo2[] Listado_Areas()
-    {
-        return new ERP_HistoriasClinicas.RecursosHumanos_LS().Listado_Areas();
-    }
-    [WebMethod]
     public int Insertar_Area(decimal ID_AREA, string NOMBRE_AREA, string GERENCIA_AREA, string DESCRIPCION_AREA, string ESTADO_AREA)
     {
-        return new ERP_HistoriasClinicas.RecursosHumanos_LS().insertar_area(ID_AREA, NOMBRE_AREA,GERENCIA_AREA,DESCRIPCION_AREA,ESTADO_AREA);
+        return new ERP_HistoriasClinicas.RecursosHumanos_LS().insertar_area(ID_AREA, NOMBRE_AREA, GERENCIA_AREA, DESCRIPCION_AREA, ESTADO_AREA);
     }
 
 }
