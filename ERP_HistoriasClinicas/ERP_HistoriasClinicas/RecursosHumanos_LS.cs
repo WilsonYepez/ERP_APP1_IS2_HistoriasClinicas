f4a31b25349a90cf078c03822f275b044c60ce8e
WILSON RAUL YEPEZ PONCE <wryepezp@utn.edu.ec>
2018-01-10 04:43:46 -0500

    v1.8 Crud Actualizados

--- ERP_HistoriasClinicas/ERP_HistoriasClinicas/RecursosHumanos_LS.cs
+++ ERP_HistoriasClinicas/ERP_HistoriasClinicas/RecursosHumanos_LS.cs
@@ -89,19 +89,32 @@ namespace ERP_HistoriasClinicas
             return 0;
 
         }
-        public int actualizar_area(decimal ID_AREA, string NOMBRE_AREA, string GERENCIA_AREA, string DESCRIPCION_AREA, string ESTADO_AREA)
+        public int update_area(decimal ID_AREA, string NOMBRE_AREA, string GERENCIA_AREA, string DESCRIPCION_AREA, string ESTADO_AREA)
         {
             using (var ctx = new ERP_HistoriasClinicasEntities())
             {
-                ctx.trx_Insert_AreaTrabajo(ID_AREA, NOMBRE_AREA, GERENCIA_AREA, DESCRIPCION_AREA, ESTADO_AREA);
-                return 0;
+                var d = ctx.AREA_TRABAJO.Find(ID_AREA);
+                if (d != null)
+                {
+                    d.NOMBRE_AREA = NOMBRE_AREA;
+                    d.ESTADO_AREA = ESTADO_AREA;
+                    d.DESCRIPCION_AREA = DESCRIPCION_AREA;
+                    d.GERENCIA_AREA = GERENCIA_AREA;
+                }
+                ctx.SaveChanges();
             }
+            return 0;
         }
         public int delete_area(decimal ID_AREA)
         {
             using (var ctx = new ERP_HistoriasClinicasEntities())
             {
-                ctx.trx_Remove_AreaTrabajo(ID_AREA);
+                var d = ctx.AREA_TRABAJO.Find(ID_AREA);
+                if (d != null)
+                {
+                    ctx.AREA_TRABAJO.Remove(d);
+                }
+                ctx.SaveChanges();
             }
             return 0;
         }
@@ -150,7 +163,6 @@ namespace ERP_HistoriasClinicas
         {
             using (var ctx = new ERP_HistoriasClinicasEntities())
             {
-
                 var d = new EMPLEADO();
                 d.CEDULA_EMP = CEDULA_EMP;
                 d.CARGO_US = CARGO_US;
@@ -162,7 +174,147 @@ namespace ERP_HistoriasClinicas
             return 0;
 
         }
-        
+        public int update_empleado(string CEDULA_EMP, decimal ID_AREA, string CARGO_US, string PASWORD_US)
+        {
+            using (var ctx = new ERP_HistoriasClinicasEntities())
+            {
+                var d = ctx.EMPLEADO.Find(CEDULA_EMP);
+                if (d != null)
+                {
+                    d.CARGO_US = CARGO_US;
+                    d.PASWORD_US = PASWORD_US;
+                    d.ID_AREA = ID_AREA;
+                }
+                ctx.SaveChanges();
+            }
+            return 0;
+        }
+        public int delete_empleado(string CEDULA_EMP)
+        {
+            using (var ctx = new ERP_HistoriasClinicasEntities())
+            {
+                var d = ctx.EMPLEADO.Find(CEDULA_EMP);
+                if (d != null)
+                {
+                    ctx.EMPLEADO.Remove(d);
+                }
+                ctx.SaveChanges();
+            }
+            return 0;
+        }
+        //PERSONA
+        public Persona2[] Listado_Persona()
+        {
+            using (var ctx = new ERP_HistoriasClinicasEntities())
+            {
+                var lst = from d in ctx.PERSONA
+                          select d;
+                Persona2[] r = new Persona2[lst.Count()];
+                int i = 0;
+                foreach (var d in lst)
+                {
+                    r[i++] = new Persona2
+                    {
+                        CEDULA_EMP=d.CEDULA_EMP,
+                        ACTIVO_EMP=d.ACTIVO_EMP,
+                        CIUDADNACI_EMP=d.CIUDADNACI_EMP,
+                        DIRECCIO_EMP=d.DIRECCIO_EMP,
+                        EMAIL_EMP=d.EMAIL_EMP,
+                        ESTADO_EMP=d.ESTADO_EMP,
+                        NACIFECHA_EMP=d.NACIFECHA_EMP,
+                        NACIONALIDAD_EMP=d.NACIONALIDAD_EMP,
+                        NOMBRES_EMP=d.NOMBRES_EMP,
+                        TELEFONO_EMP=d.TELEFONO_EMP,
+                        TIPO_EMP=d.TIPO_EMP
+                    };
+                }
+                return r;
+            }
+        }
+        public Persona2 Persona_Cedula(string CEDULA_EMP)
+        {
+            using (var ctx = new ERP_HistoriasClinicasEntities())
+            {
+                var d = ctx.PERSONA.Find(CEDULA_EMP);
+                Persona2 r = null;
+                if (d != null)
+                {
+                    r = new Persona2
+                    {
+                        CEDULA_EMP = d.CEDULA_EMP,
+                        ACTIVO_EMP = d.ACTIVO_EMP,
+                        CIUDADNACI_EMP = d.CIUDADNACI_EMP,
+                        DIRECCIO_EMP = d.DIRECCIO_EMP,
+                        EMAIL_EMP = d.EMAIL_EMP,
+                        ESTADO_EMP = d.ESTADO_EMP,
+                        NACIFECHA_EMP = d.NACIFECHA_EMP,
+                        NACIONALIDAD_EMP = d.NACIONALIDAD_EMP,
+                        NOMBRES_EMP = d.NOMBRES_EMP,
+                        TELEFONO_EMP = d.TELEFONO_EMP,
+                        TIPO_EMP = d.TIPO_EMP
+                    };
+                }
+                return r;
+            }
+        }
+        public int insertar_Persona( string CEDULA_EMP , string NOMBRES_EMP , System.DateTime NACIFECHA_EMP , string NACIONALIDAD_EMP, string DIRECCIO_EMP , 
+            string TELEFONO_EMP , string EMAIL_EMP , string ESTADO_EMP , string TIPO_EMP , string ACTIVO_EMP , string CIUDADNACI_EMP )
+        {
+            using (var ctx = new ERP_HistoriasClinicasEntities())
+            {
+                var d = new PERSONA();
+                d.CEDULA_EMP = CEDULA_EMP;
+                d.NOMBRES_EMP = NOMBRES_EMP;
+                d.NACIFECHA_EMP = NACIFECHA_EMP;
+                d.NACIONALIDAD_EMP = NACIONALIDAD_EMP;
+                d.DIRECCIO_EMP = DIRECCIO_EMP;
+                d.TELEFONO_EMP = TELEFONO_EMP;
+                d.EMAIL_EMP = EMAIL_EMP;
+                d.ESTADO_EMP = ESTADO_EMP;
+                d.TIPO_EMP = TIPO_EMP;
+                d.ACTIVO_EMP = ACTIVO_EMP;
+                d.CIUDADNACI_EMP = CIUDADNACI_EMP;
+                ctx.PERSONA.Add(d);
+                ctx.SaveChanges();
+            }
+            return 0;
 
+        }
+        public int update_Persona(string CEDULA_EMP, string NOMBRES_EMP, System.DateTime NACIFECHA_EMP, string NACIONALIDAD_EMP, string DIRECCIO_EMP,
+            string TELEFONO_EMP, string EMAIL_EMP, string ESTADO_EMP, string TIPO_EMP, string ACTIVO_EMP, string CIUDADNACI_EMP)
+        {
+            using (var ctx = new ERP_HistoriasClinicasEntities())
+            {
+                var d = ctx.PERSONA.Find(CEDULA_EMP);
+                if (d != null)
+                {
+                    d.NOMBRES_EMP = NOMBRES_EMP;
+                    d.NACIFECHA_EMP = NACIFECHA_EMP;
+                    d.NACIONALIDAD_EMP = NACIONALIDAD_EMP;
+                    d.DIRECCIO_EMP = DIRECCIO_EMP;
+                    d.TELEFONO_EMP = TELEFONO_EMP;
+                    d.EMAIL_EMP = EMAIL_EMP;
+                    d.ESTADO_EMP = ESTADO_EMP;
+                    d.TIPO_EMP = TIPO_EMP;
+                    d.ACTIVO_EMP = ACTIVO_EMP;
+                    d.CIUDADNACI_EMP = CIUDADNACI_EMP;
+                }
+                ctx.SaveChanges();
+            }
+            return 0;
+        }
+        public int delete_Persona(string CEDULA_EMP)
+        {
+            using (var ctx = new ERP_HistoriasClinicasEntities())
+            {
+                var d = ctx.PERSONA.Find(CEDULA_EMP);
+                if (d != null)
+                {
+                    ctx.PERSONA.Remove(d);
+                }
+                ctx.SaveChanges();
+            }
+            return 0;
+        }
     }
 }
