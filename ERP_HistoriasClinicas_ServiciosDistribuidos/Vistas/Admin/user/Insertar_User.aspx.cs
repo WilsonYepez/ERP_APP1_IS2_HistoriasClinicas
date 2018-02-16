9641dc25df68635bdc0ead41c32c968cb305a8a4
WILSON RAUL YEPEZ PONCE <wryepezp@utn.edu.ec>
2018-02-16 04:16:23 -0500

    qwe

--- ERP_HistoriasClinicas_ServiciosDistribuidos/Vistas/Admin/user/Insertar_User.aspx.cs
+++ ERP_HistoriasClinicas_ServiciosDistribuidos/Vistas/Admin/user/Insertar_User.aspx.cs
@@ -21,12 +21,26 @@ public partial class Vistas_Admin_user_Crud_User : System.Web.UI.Page
 
     protected void ADD_btn_User_Click(object sender, EventArgs e)
     {
-        String cedula = DropDownList2.SelectedItem.Value;
-        DropDownList2.Items.RemoveAt(DropDownList2.SelectedIndex);
-        decimal id_area = Convert.ToDecimal(DropDownList1.SelectedItem.Value);
-        String Cargo = DropDownList3.SelectedItem.Value;
-        obj.insertar_Empleado( cedula, id_area , Cargo , TextBox1.Text,"ACTIVO");
-        Response.Redirect("./Default.aspx");
+        try{
+            if (TextBox1.Text != "")
+            {
+                String cedula = DropDownList2.SelectedItem.Value;
+                DropDownList2.Items.RemoveAt(DropDownList2.SelectedIndex);
+                decimal id_area = Convert.ToDecimal(DropDownList1.SelectedItem.Value);
+                String Cargo = DropDownList3.SelectedItem.Value;
+                obj.insertar_Empleado(cedula, id_area, Cargo, TextBox1.Text, "ACTIVO");
+                Response.Redirect("./Insertar_User.aspx");
+            }
+            else{
+                Page.ClientScript.RegisterStartupScript(this.GetType(), "Script", "<script>alert('Error: Ingrese un password')</script>");
+
+            }
+        }
+        catch(Exception ex)
+        {
+            Page.ClientScript.RegisterStartupScript(this.GetType(), "Script", "<script>alert('Error: "+ex+"')</script>");
+        }
+        
     }
 
 }
